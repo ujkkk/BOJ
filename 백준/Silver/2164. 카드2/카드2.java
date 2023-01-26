347528b36811b7d3181754dfb11defecd338705f
ujkkk <88374384+ujkkk@users.noreply.github.com>
2023-01-26 18:35:49 +0900

    [Silver IV] Title: 카드2, Time: 276 ms, Memory: 56904 KB -BaekjoonHub

---
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+	
+		Scanner sc = new Scanner(System.in);
+		int N = Integer.parseInt(sc.nextLine());
+		int n= 0;
+		int last = 0;
+		Queue<Integer> que = new LinkedList<>();
+		//1~N 까지 큐에 데이터 삽입
+		for(int i=0; i< N; i++) {
+			que.add(i+1);
+		}
+		while(true) {
+			last = que.remove();
+			if(que.peek() == null) break;
+			n  = que.remove();
+			que.add(n);
+		}
+		System.out.println(last);
+		
+	}
+	
+}
