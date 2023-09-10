523962cfe8d581b33e111003ba29c3f4c7efe2a3
UJeans <88374384+ujkkk@users.noreply.github.com>
2023-09-10 15:22:32 +0900

    [Silver II] Title: 쇠막대기, Time: 200 ms, Memory: 16888 KB -BaekjoonHub

--- 백준/Silver/10799. 쇠막대기/쇠막대기.java
+++ 백준/Silver/10799. 쇠막대기/쇠막대기.java
@@ -11,38 +11,47 @@ public class Main {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         Stack<Integer> stack = new Stack<Integer>();
+        // 총 막대기 수
         int sum = 0;
-        int lazer = 0;
-        int cur = 0;
+        // 레이저의 수
+        int laser = 0;
+        int curItem = 0;
 
         String str = (br.readLine());
-        // 스택 완성
+        /**
+         *  '(' => -1
+         *  ') => 0
+         *  레이저 => 1 이상의 수
+         *  레이저의 수를 스택에 넣기 위해 String<Integer> 사용
+         */
         for(int i=0; i< str.length(); i++) {
             if(str.charAt(i) == '('){
                 stack.push(-1);
             }
             if (str.charAt(i) == ')'){
+                // 레이저가 나올 때
                 if(stack.peek() == -1){
                     stack.pop();
                     stack.push(1);
                 }
+                // 막대기의 끝 발견
                 else{
-                    lazer = 0;
-                    while(!((cur = stack.pop()) == -1)){
-                        if(cur >= 1){
-                            lazer += cur;
-                        }
+                    laser = 0;
+                    // ')' 를 만나면 '('를 만날 때까지 pop,
+                    while(!((curItem = stack.pop()) == -1)){
+                        // 중간에 만나는 레이저의 수 만큼 누적해서 더함
+
+                        laser += curItem;
+
                     }
-                    sum += lazer +1;
-                    stack.push(lazer);
+                    // 현재 막대기가 잘리지는 조각 수 = 만난 레이저의 개수 +1)
+                    sum += laser +1;
+                    // pop한 레이저의 수를 더해서 push
+                    stack.push(laser);
                 }
             }
         }
-
-
         System.out.println(sum);
-
-
     }
 
 
@@ -50,3 +59,4 @@ public class Main {
 
 
 }
+
