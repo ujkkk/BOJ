7b6df5b4acc502b1a2120d5309f345ab38d97702
UJeans <88374384+ujkkk@users.noreply.github.com>
2023-10-19 01:20:44 +0900

    [Silver I] Title: 스티커, Time: 972 ms, Memory: 135896 KB -BaekjoonHub

--- 백준/Silver/9465. 스티커/스티커.java
+++ 백준/Silver/9465. 스티커/스티커.java
@@ -24,8 +24,6 @@ public class Main {
             for(int i=0; i<N; i++) {
                 nums[0][i] = Integer.parseInt(st1.nextToken());
                 nums[1][i] = Integer.parseInt(st2.nextToken());
-//                dp[0][i] = nums[0][i];
-//                dp[1][i] = nums[1][i];
             }
 
             /* solution */
@@ -35,30 +33,18 @@ public class Main {
                     dp[1][0] = nums[1][0];
                     continue;
                 }
-                // 3가지 경우의 수 확인
-                // 1열 (x-2,y) (x-2,y+1) (x-1, y+1)
-                int [] dx1 = new int[] {-2,-2,-1};
-                int [] dy1 = new int [] {0, 1, 1};
-                for(int j=0; j<dx1.length; j++) {
-                    int x = i +dx1[j];
-                    int y = dy1[j];
-                    if(x>=0 && y>=0) {
-                        dp[0][i] = Math.max(dp[0][i], nums[0][i]+dp[y][x]);
-                    }
 
-                }
-
-                // 2열 (x-2,y) (x-2,y-1) (x-1, y-1)
-                int [] dx2 = new int[] {-2,-2,-1};
-                int [] dy2 = new int [] {0, -1, -1};
-                for(int j=0; j<dx2.length; j++) {
-                    int x = i +dx2[j];
-                    int y = 1+ dy2[j];
+                int [] dx = new int[] {-2,-1};
+                for(int j=0; j<dx.length; j++) {
+                    int x = i +dx[j];
+                    int y = 1;
                     if(x>=0 && y>=0) {
-                        dp[1][i] = Math.max(dp[1][i], nums[1][i]+ dp[y][x]);
+                        // 0행
+                        dp[0][i] = Math.max(dp[0][i], nums[0][i]+dp[1][x]);
+                        // 1행
+                        dp[1][i] = Math.max(dp[1][i], nums[1][i]+ dp[0][x]);
                     }
                 }
-
             }
 
             //max값 찾기
