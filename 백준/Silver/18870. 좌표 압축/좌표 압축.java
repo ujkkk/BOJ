3ba2abe5932d69c7ff008cd34d3893eb743c6f91
UJeans <88374384+ujkkk@users.noreply.github.com>
2023-11-17 00:38:26 +0900

    [Silver II] Title: 좌표 압축, Time: 1804 ms, Memory: 266528 KB -BaekjoonHub

--- 백준/Silver/18870. 좌표 압축/좌표 압축.java
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.java
@@ -1,11 +1,6 @@
-import java.util.ArrayList;
-import java.util.Comparator;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.Iterator;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Queue;
 import java.util.StringTokenizer;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
@@ -14,8 +9,6 @@ import java.io.OutputStreamWriter;
 
 class Main
 {
-	
-	
 	public static void main(String args[]) throws Exception
 	{
 		
@@ -25,29 +18,26 @@ class Main
 		int N = Integer.parseInt(br.readLine());
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		
-		// 중복값을 제거한 값으로 채워 리스트 생성
+		// 리스트 생성
 		HashSet<Integer> duplicate = new HashSet<>();
 		int [] input = new int [N];
-		List<Integer> noDuplicateData = new ArrayList<>();
+		int [] sorted = new int [N];
+		
 		for(int i=0; i<N; i++) {
 			int num = Integer.parseInt(st.nextToken());
-			input[i] = num;
-			if(duplicate.contains(num))
-				continue;
-			duplicate.add(num);
-			noDuplicateData.add(num);
-			
+			input[i] = sorted[i] = num;
 		}
 		
 		//리스트 정렬
-		noDuplicateData.sort(Comparator.naturalOrder());
+		Arrays.sort(sorted);
 		
 		//결과 해쉬맵에 넣기
 		HashMap<Integer, Integer> resultMap = new HashMap<>();
-		Iterator<Integer> iterator = noDuplicateData.iterator();
 		int index =0;
-		while(iterator.hasNext()) {
-			resultMap.put(iterator.next(), index++);
+		for(int num : sorted) {
+			if(!resultMap.containsKey(num)) {
+				resultMap.put(num, index++);
+			}
 		}
 		
 		//출력
