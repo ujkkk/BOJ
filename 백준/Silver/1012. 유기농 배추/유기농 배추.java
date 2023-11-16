c468e3915316ef471b42f141dd5d1385e4387dd7
UJeans <88374384+ujkkk@users.noreply.github.com>
2023-11-16 23:54:20 +0900

    [Silver II] Title: 유기농 배추, Time: 192 ms, Memory: 16484 KB -BaekjoonHub

---
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.java
@@ -0,0 +1,95 @@
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+class Main
+{
+	public static void main(String args[]) throws Exception
+	{
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int T = Integer.parseInt(br.readLine());
+		
+		
+		for(int t = 0; t<T; t++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			// 가로 길이
+			int M = Integer.parseInt(st.nextToken());
+			// 세로 길이
+			int N = Integer.parseInt(st.nextToken());
+			// 배추 위치 개
+			int K = Integer.parseInt(st.nextToken());
+			
+			// 배추밭 정보 생성
+			boolean isBaecu [][] = new boolean[N+2][M+2];
+			for(int k=0; k<K; k++) {
+				// 배추 위치 정보
+				st = new StringTokenizer(br.readLine());
+				int x = Integer.parseInt(st.nextToken());
+				int y = Integer.parseInt(st.nextToken());
+				isBaecu[y+1][x+1] = true;
+			}
+			
+			bw.write(getCount(isBaecu) + "\n");
+		
+			
+		}
+		bw.flush();
+	}
+	
+	public static int getCount(boolean [][] isBaecu) {
+		int [] dx = {0,1,0,-1};
+		int [] dy = {1,0, -1,0};
+		
+		int row = isBaecu.length;
+		int col = isBaecu[0].length;
+		
+		int count = 0;
+		Queue<Point> que = new LinkedList<>();
+		boolean [][] isVisited = new boolean[row][col];
+		
+		for(int y=1; y<row; y++) {
+			for(int x = 1; x<col; x++) {
+				// 새로운 그룹 발견
+				if(!isVisited[y][x] && isBaecu[y][x] == true) {
+					count++;
+					que.add(new Point(x,y));
+					isVisited[y][x] = true;
+					
+					while(!que.isEmpty()) {
+						Point cur = que.poll();
+						
+						for(int i=0; i<4; i++) {
+							int nextX = cur.x+dx[i];
+							int nextY = cur.y+dy[i];
+					
+							if(!isVisited[nextY][nextX]&& isBaecu[nextY][nextX] == true) {
+								que.add(new Point(nextX, nextY));
+								isVisited[nextY][nextX] = true;
+							}
+						}
+					}
+				}
+			}
+		}
+		return count;
+	}
+
+}
+
+class Point{
+	int x;
+	int y;
+	public Point(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+}
