ccf7b84a8891f87edd103c479a488d415082d05f
UJeans <88374384+ujkkk@users.noreply.github.com>
2023-11-21 02:21:29 +0900

    [Silver I] Title: 케빈 베이컨의 6단계 법칙, Time: 140 ms, Memory: 15756 KB -BaekjoonHub

--- 백준/Silver/1389. 케빈 베이컨의 6단계 법칙/케빈 베이컨의 6단계 법칙.java
+++ 백준/Silver/1389. 케빈 베이컨의 6단계 법칙/케빈 베이컨의 6단계 법칙.java
@@ -25,7 +25,7 @@ public class Main {
 
         friends = new ArrayList[N+1];
         kevinValues = new int[N+1][N+1];
-        long min = Long.MAX_VALUE;
+
         for(int i=1; i<=N; i++)
             friends[i] = new ArrayList<>();
 
@@ -39,27 +39,24 @@ public class Main {
             friends[end].add(start);
         }
 
+        // 케빈 값들 초기화
+        for(int row=0; row< kevinValues.length; row++){
+            for(int col=0; col<kevinValues[0].length; col++){
+                kevinValues[row][col] = Integer.MAX_VALUE;
+            }
+        }
         kevinMin = Long.MAX_VALUE;
+
         // 차례대로 케빈 값을 구함
         for(int i=1; i<=N; i++){
             int sum = 0;
-            stack = new Stack<>();
-            isVisted = new boolean[N+1];
-            for(int row=0; row< kevinValues.length; row++){
-                for(int col=0; col<kevinValues[0].length; col++){
-                    kevinValues[row][col] = Integer.MAX_VALUE;
-                }
-            }
-
-            stack.add(i);
-            isVisted[i] = true;
-
             getKevinValues(i);
 
             // 총 케빈 베이컨의 합
             for(int kevin : kevinValues[i])
                 sum += kevin;
 
+            // 작을 때 갱신
             if(kevinMin > sum){
                 ans = i;
                 kevinMin = sum;
@@ -78,6 +75,7 @@ public class Main {
 
         while(!que.isEmpty()){
             Node curNode = que.poll();
+            // 갱신할 수 있을 때만 다시 갱신
             if(kevinValues[start][curNode.x] > curNode.depth){
                 kevinValues[start][curNode.x] = curNode.depth;
 
