9ede3d9dfcd167191f3297fb148972bcbbaa238a
UJeans <88374384+ujkkk@users.noreply.github.com>
2023-11-29 00:15:16 +0900

    [Gold V] Title: 배, Time: 208 ms, Memory: 16740 KB -BaekjoonHub

--- 백준/Gold/1092. 배/배.java
+++ 백준/Gold/1092. 배/배.java
@@ -1,89 +1,109 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import java.io.*;
 import java.util.*;
 
 public class Main{
+    static BufferedReader br;
+    static BufferedWriter bw;
+    static List<Crane> crane;
+    static List<Integer> boxs;
+    static int N;
+    static int M;
+    static int ans;
 
-    public static void main(String [] args) throws Exception{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    public Main(){
+        br = new BufferedReader(new InputStreamReader(System.in));
+        bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    }
 
-        List<Crain> crains = new ArrayList<>();
-        int [] boxs;
+    public static void run() throws Exception {
+        input();
+        solution();
+        output();
+    }
 
-        int N = Integer.parseInt(br.readLine());
+    public static void input() throws Exception {
+        crane = new ArrayList<>();
+        boxs = new ArrayList<>();
 
+        N = Integer.parseInt(br.readLine());
         StringTokenizer st = new StringTokenizer(br.readLine());
         // 상자 제한 무게 입력
         for(int i=0; i<N; i++){
             int limit = Integer.parseInt(st.nextToken());
-            crains.add(new Crain(limit));
+            crane.add(new Crane(limit));
         }
 
-        int M = Integer.parseInt(br.readLine());
+        M = Integer.parseInt(br.readLine());
         st = new StringTokenizer(br.readLine());
-        boxs = new int[M];
         // 박스 무게 입력
         for(int i=0; i<M; i++){
-            boxs[i] = Integer.parseInt(st.nextToken());
+            boxs.add(Integer.parseInt(st.nextToken()));
         }
+    }
 
-        // 오름차순 정렬
-        Arrays.sort(boxs);
+    public static void solution(){
+        // 박스 내림차순 정렬
+        boxs.sort(Comparator.reverseOrder());
         // 크레인 내림차순 정렬
-        crains.sort(new Comparator<Crain>() {
+        crane.sort(new Comparator<Crane>() {
             @Override
-            public int compare(Crain o1, Crain o2) {
-                return o1.limit- o2.limit;
+            public int compare(Crane o1, Crane o2) {
+                return o2.limit- o1.limit;
             }
         });
+
         // 크레인이 이동 시켜야 할 상자 갯수
         int result [] = new int[N];
         Arrays.fill(result, Integer.MAX_VALUE);
 
-        for(int m=M-1; m>=0; m--){
-            int crainNumber = (N-1);
-            int c;
-            for(c = N-1; c>=0; c--){
-                Crain curCrain = crains.get(c);
-                if(curCrain.limit < boxs[m])
+        int boxIdx, craneIdx;
+        for(boxIdx=0; boxIdx< M; boxIdx++){
+            int crainNumber = 0;
+            for(craneIdx = 0; craneIdx<N; craneIdx++){
+                Crane curCrain = crane.get(craneIdx);
+                if(curCrain.limit < boxs.get(boxIdx))
                     break;
                 // 작은 count를 가지는 crain찾기
-                if(curCrain.count < crains.get(crainNumber).count){
-                    crainNumber = c;
+                if(curCrain.count <= crane.get(crainNumber).count){
+                    crainNumber = craneIdx;
                 }
             }
-            if(c==N-1){
-                System.out.println("-1");
+            // 어떠한 크레인으로도 들 수 없을 때
+            if(craneIdx==0){
+                ans = -1;
                 return;
             }
-            crains.get(crainNumber).addBox();
-
+            // 해당 크레인의 count 1증가
+            crane.get(crainNumber).addBox();
         }
 
         // 가장 큰 count 구하기
         int max = Integer.MIN_VALUE;
-        for(Crain c : crains){
+        for(Crane c : crane){
             if(c.count > max){
                 max = c.count;
             }
         }
-        bw.write(max+"\n");
+        ans = max;
+    }
+    public static void output() throws Exception{
+        bw.write(ans+"\n");
         bw.flush();
 
         bw.close();
         br.close();
+    }
 
+    public static void main(String [] args) throws Exception{
+        new Main().run();
     }
 }
 
-class Crain {
+class Crane {
     int limit;
     int count;
 
-    public Crain(int limit){
+    public Crane(int limit){
         this.limit = limit;
         this.count = 0;
     }
@@ -91,5 +111,4 @@ class Crain {
     public void addBox(){
         count++;
     }
-
 }
