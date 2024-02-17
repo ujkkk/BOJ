24ff8370db8bb9667e2c19c901dc22065b7af335
김유진 <88374384+ujkkk@users.noreply.github.com>
2024-02-17 14:52:10 +0900

    [Silver I] Title: RGB거리, Time: 140 ms, Memory: 14604 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.java
+++ 백준/Silver/1149. RGB거리/RGB거리.java
@@ -1,39 +1,58 @@
 import java.io.*;
-import java.util.*;
+import java.util.StringTokenizer;
 
-public class Main{
-
-
-    public static void main(String [] args) throws Exception{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-        int N = Integer.parseInt(br.readLine());
-        int [][] rgbs = new int[N+1][3];
-        for(int i=1; i<=N; i++){
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            rgbs[i][0] = Integer.parseInt(st.nextToken());
-            rgbs[i][1] = Integer.parseInt(st.nextToken());
-            rgbs[i][2] = Integer.parseInt(st.nextToken());
-
-        }
-
-        long dp[][] = new long[N+1][3];
-        dp[1][0] = rgbs[1][0];
-        dp[1][1] = rgbs[1][1];
-        dp[1][2] = rgbs[1][2];
-
-        for(int i=2; i<=N; i++){
-            dp[i][0] =Math.min(dp[i-1][1], dp[i -1][2]) + rgbs[i][0];
-            dp[i][1] = Math.min(dp[i-1][0], dp[i -1][2]) + rgbs[i][1];
-            dp[i][2] = Math.min(dp[i-1][1], dp[i -1][0])+ rgbs[i][2];
-        }
-
-        long min = Long.MAX_VALUE;
-        for(long value : dp[N]){
-            min = Math.min(min, value);
-        }
-        bw.write(min+"\n");
-        bw.flush();
-    }
+public class Main {
+	
+	public static BufferedWriter bw;
+	public static BufferedReader br;
+	
+	static int [] dp;
+	static int N;
+	
+	public static void main(String[] args) throws Exception {
+		
+		BufferedReader br  = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	
+		int N = Integer.parseInt(br.readLine());
+		int rgb [][] = new int[N+1][3];
+		int dp [][] = new int[N+1][3];
+		
+		StringTokenizer st;
+		for(int i=1; i<=N; i++) {
+			st = new StringTokenizer(br.readLine());
+			for(int j=0; j<3; j++) {
+				rgb[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+		
+		dp[1][0] = rgb[1][0];
+		dp[1][1] = rgb[1][1];
+		dp[1][2] = rgb[1][2];
+		
+		for(int i=2; i<=N; i++) {
+			for(int j=0; j<3; j++) {
+				if(j==0)
+					dp[i][j] = Math.min(dp[i-1][1], dp[i-1][2]) ;
+				else if(j==1)
+					dp[i][j] = Math.min(dp[i-1][0], dp[i-1][2]);
+				else
+					dp[i][j] = Math.min(dp[i-1][0], dp[i-1][1]);
+				
+				dp[i][j] += rgb[i][j];
+				
+			}
+		}
+		int result = Integer.MAX_VALUE;
+		for(int i=0; i<3; i++) {
+			result = Math.min(result, dp[N][i]);
+		}
+		
+		bw.write(result+"");
+		bw.flush();
+	}
+		
 }
+
+
+
