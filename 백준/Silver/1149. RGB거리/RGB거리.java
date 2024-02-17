be1ce988edc3b8bfeca3a7749bbd1d1c39f02d3c
김유진 <88374384+ujkkk@users.noreply.github.com>
2024-02-17 14:54:31 +0900

    [Silver I] Title: RGB거리, Time: 132 ms, Memory: 14552 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.java
+++ 백준/Silver/1149. RGB거리/RGB거리.java
@@ -1,3 +1,4 @@
+
 import java.io.*;
 import java.util.StringTokenizer;
 
@@ -31,18 +32,13 @@ public class Main {
 		dp[1][2] = rgb[1][2];
 		
 		for(int i=2; i<=N; i++) {
-			for(int j=0; j<3; j++) {
-				if(j==0)
-					dp[i][j] = Math.min(dp[i-1][1], dp[i-1][2]) ;
-				else if(j==1)
-					dp[i][j] = Math.min(dp[i-1][0], dp[i-1][2]);
-				else
-					dp[i][j] = Math.min(dp[i-1][0], dp[i-1][1]);
-				
-				dp[i][j] += rgb[i][j];
-				
-			}
+			
+			dp[i][0] = Math.min(dp[i-1][1], dp[i-1][2])+ rgb[i][0] ;
+			dp[i][1] = Math.min(dp[i-1][0], dp[i-1][2])+ rgb[i][1];
+			dp[i][2] = Math.min(dp[i-1][0], dp[i-1][1])+ rgb[i][2];	
+			
 		}
+		
 		int result = Integer.MAX_VALUE;
 		for(int i=0; i<3; i++) {
 			result = Math.min(result, dp[N][i]);
