e47b0bef2c5d62842c5610b186ed8a850dd1b1b1
김유진 <88374384+ujkkk@users.noreply.github.com>
2025-01-28 19:24:56 +0900

    [level 2] Title: [PCCP 기출문제] 3번 / 충돌위험 찾기, Time: 617.01 ms, Memory: 247 MB -BaekjoonHub

--- 프로그래머스/2/340211. ［PCCP 기출문제］ 3번 ／ 충돌위험 찾기/［PCCP 기출문제］ 3번 ／ 충돌위험 찾기.java
+++ 프로그래머스/2/340211. ［PCCP 기출문제］ 3번 ／ 충돌위험 찾기/［PCCP 기출문제］ 3번 ／ 충돌위험 찾기.java
@@ -1,118 +1,63 @@
 import java.util.*;
-
 class Solution {
-    static int R;
-    static int C; 
-    static Map<Integer, Point> pointMap = new HashMap<>();
-    
-    class Point {
-        int x, y;
-        public Point(int x, int y) {
-            this.x = x;
-            this.y = y;
-        }
-        
-        @Override
-        public boolean equals(Object obj) {
-            if (this == obj) return true;
-            if (obj == null || getClass() != obj.getClass()) return false;
-            Point point = (Point) obj;
-            return x == point.x && y == point.y;
-        }
-
-        @Override
-        public int hashCode() {
-            return Objects.hash(x, y);
-        }
-    }
-    
     public int solution(int[][] points, int[][] routes) {
-        int answer = 0;
-        setBoardSize(points);
-        setPointMap(points);
+        // BFS로 최단거리 확보
+        HashMap<String, Integer> map = new HashMap();
+        int count = 0;
         
-        int longestRouteDist = 0;
-        List<List<Point>> shortestRoutes = new ArrayList<>();
-        for (int[] r : routes) {
-            List<Point> route = findShortestRoute(r);
-            shortestRoutes.add(route);     
-            longestRouteDist = (longestRouteDist < route.size()) ? route.size() : longestRouteDist;
-        }
-        
-        answer = findStrikeCount(shortestRoutes, longestRouteDist);
-        return answer;
-    }
-    
-    public void setBoardSize(int[][] points) {
-        int r = 0;
-        int c = 0;
-        for (int[] point : points) {
-            if (point[0] > r) {
-                r = point[0];
-            }
-            if (point[1] > c) {
-                c = point[1];
-            }
-        }
-        R = r;
-        C = c;
-    }
-    
-    public void setPointMap(int[][] points) {
-        for (int i = 0; i < points.length; i++) {
-            pointMap.put(i + 1, new Point(points[i][1], points[i][0]));
-        }
-    }
-    
-    public int findStrikeCount(List<List<Point>> shortestRoutes, int longestRouteDist) {
-        int totalCount = 0;
-        
-        for (int i = 0; i < longestRouteDist; i++) {
-            Map<Point, Integer> countMap = new HashMap<>();
+        for(int i=0; i<routes.length; i++){
+            List<Integer> shotList = new ArrayList();
             
-            for (List<Point> shortestRoute : shortestRoutes) {
-                Point point = (shortestRoute.size() > i) ? shortestRoute.get(i) : null;
-                if (point != null) {
-                    if (countMap.get(point) == null) {
-                        countMap.put(point, 1);
-                        continue;
-                    }
-                    countMap.put(point, countMap.get(point) + 1);
+            for(int j=0; j<routes[i].length-1; j++){
+                int s = routes[i][j] - 1;
+                int e = routes[i][j+1] - 1;
+            
+                int start = points[s][0]*1000 + points[s][1];
+                int end = points[e][0]*1000 + points[e][1];
+            
+                shortestRoute(start, end, shotList);
+            }
+            
+            String str = "";
+            for(int p=0; p<shotList.size(); p++){
+                str = p + ":" + shotList.get(p);
+                
+                if(map.get(str) == null){
+                    map.put(str, 1);
+                    continue;
                 }
-            }
-            
-            for (Integer count : countMap.values()) {
-                if (count > 1) {
-                    totalCount += 1;
+                
+                int value = map.get(str);
+                if(value == 1){
+                    count++;
                 }
+                map.put(str, value+1);
             }
         }
-        return totalCount;
+        
+        return count;
     }
     
-    public List<Point> findShortestRoute(int[] routePoints) {
-        List<Point> shortestRoute = new ArrayList<>();
-        for (int i = 1; i < routePoints.length; i++) {
-            Point start = pointMap.get(routePoints[i - 1]);
-            Point end = pointMap.get(routePoints[i]);
-            int r = start.y;
-            int c = start.x;
-            
-            if (shortestRoute.isEmpty()) {
-                shortestRoute.add(new Point(r, c));
-            }
-            
-            while (r != end.y) {
-                r += (r > end.y) ? -1 : 1;
-                shortestRoute.add(new Point(r, c));
-            }
-            
-            while (c != end.x) {
-                c += (c > end.x) ? -1 : 1;
-                shortestRoute.add(new Point(r, c));
-            }
+    private void shortestRoute(int start, int end, List<Integer> shotList){
+        int r = start/1000;
+        int c = start%1000;
+        
+        int er = end/1000;
+        int ec = end%1000;
+        
+        // 맨 처음 경로에서 시작했을 때 
+        if(shotList.size() == 0){
+            shotList.add(start);
         }
-        return shortestRoute;
+        while(r != er){
+            r = r>er ? r-1: r+1;
+            shotList.add(r*1000 + c);
+        }
+        
+        while(c != ec){
+            c = c>ec? c-1: c+1;
+            shotList.add(r*1000 + c);
+        }
+        
     }
-    
 }
