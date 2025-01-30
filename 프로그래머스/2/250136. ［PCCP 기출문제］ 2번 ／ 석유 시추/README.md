56d27fcc96abe186aab15ae76db1a84c340f9e00
김유진 <88374384+ujkkk@users.noreply.github.com>
2025-01-30 16:05:33 +0900

    [level 2] Title: [PCCP 기출문제] 2번 / 석유 시추, Time: 107.02 ms, Memory: 74.1 MB -BaekjoonHub

--- 프로그래머스/2/250136. ［PCCP 기출문제］ 2번 ／ 석유 시추/README.md
+++ 프로그래머스/2/250136. ［PCCP 기출문제］ 2번 ／ 석유 시추/README.md
@@ -1,10 +1,10 @@
 # [level 2] [PCCP 기출문제] 2번 / 석유 시추 - 250136 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/250136) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/250136#) 
 
 ### 성능 요약
 
-메모리: 74.2 MB, 시간: 100.44 ms
+메모리: 74.1 MB, 시간: 107.02 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 01월 30일 15:48:23
+2025년 01월 30일 16:05:31
 
 ### 문제 설명
 
--- 프로그래머스/2/250136. ［PCCP 기출문제］ 2번 ／ 석유 시추/［PCCP 기출문제］ 2번 ／ 석유 시추.java
+++ 프로그래머스/2/250136. ［PCCP 기출문제］ 2번 ／ 석유 시추/［PCCP 기출문제］ 2번 ／ 석유 시추.java
@@ -4,71 +4,60 @@ class Solution {
     
     static int N, M;
     static int [][] groupMap;
-    
+    static int [] counts;
     public int solution(int[][] land) {
+        // 1. BFS로 그룹번호 map, <number, count> 얻기
         N = land.length;
         M = land[0].length;
         
         boolean [][] isVisited = new boolean[N][M];
-        HashMap<Integer, Integer> countMap = new HashMap<>();
-        int groupNumber = 1;
+        counts = new int[M];
         
-        // 그룹화 및 석유량 계산
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < M; j++) {
-                if (isVisited[i][j] || land[i][j] == 0) {
+        for(int i=0; i<N; i++){
+            for(int j=0; j<M; j++){
+                if(isVisited[i][j] || land[i][j] == 0){
                     continue;
                 }
-                int count = bfs(groupNumber, i, j, isVisited, land);
-                countMap.put(groupNumber++, count);
+                bfs(i, j, isVisited,land);
             }
         }
-    
-        // 열을 탐색하면서 최대 석유량 계산
+        
+        // 2. 하나씩 열 탐색하면서 최대치 구하기
         int max = 0;
-        for (int j = 0; j < M; j++) {
-            Set<Integer> visitedGroups = new HashSet<>();
-            int curMax = 0;
-            
-            for (int i = 0; i < N; i++) {
-                if (land[i][j] == 0) continue;
-                
-                int curGroup = land[i][j];
-                if (!visitedGroups.contains(curGroup)) {
-                    visitedGroups.add(curGroup);
-                    curMax += countMap.get(curGroup);
-                }
-            }
-            
-            max = Math.max(max, curMax);
+        for(int j=0; j<M; j++){
+            //System.out.println(counts[j]);
+            max = Math.max(max, counts[j]);
         }
         
         return max;
     }
     
-    int bfs(int groupNumber, int r, int c, boolean [][] isVisited, int[][] land) {
-        Queue<Point> que = new LinkedList<>();
-        int count = 0;
+    void bfs(int r, int c, boolean [][] isVisited, int[][] land){
+        Queue<Point> que = new LinkedList();
         
-        que.add(new Point(r, c));
+        que.add(new Point(r,c));
         isVisited[r][c] = true;
         
-        int[] dr = {0, -1, 0, 1};
-        int[] dc = {1, 0, -1, 0};
+        int [] dr = {0, -1, 0, 1};
+        int [] dc = {1, 0, -1, 0};
         
-        while (!que.isEmpty()) {
+        // 방문한 열을 저장할 set
+        Set<Integer> set = new HashSet();
+        int count = 0;
+        
+        while(!que.isEmpty()){
             Point cur = que.poll();
-            land[cur.r][cur.c] = groupNumber;
             count++;
+            set.add(cur.c);
             
-            for (int i = 0; i < 4; i++) {
+            for(int i=0; i<4; i++){
                 int nr = cur.r + dr[i];
                 int nc = cur.c + dc[i];
                 
-                if (nr < 0 || nr >= N || nc < 0 || nc >= M) {
+                if(nr <0|| nr >=N || nc <0 || nc >=M){
                     continue;
                 }
-                if (isVisited[nr][nc] || land[nr][nc] == 0) {
+                if(isVisited[nr][nc] || land[nr][nc] == 0){
                     continue;
                 }
                 
@@ -76,14 +65,19 @@ class Solution {
                 isVisited[nr][nc] = true;
             }
         }
-        return count;
+        
+        for(int idx : set){
+            counts[idx] += count;
+        }
+        
     }
 }
 
-class Point {
-    int r, c;
+class Point{
+    int r;
+    int c;
     
-    public Point(int r, int c) {
+    public Point(int r, int c){
         this.r = r;
         this.c = c;
     }
