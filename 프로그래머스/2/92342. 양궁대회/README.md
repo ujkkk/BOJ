abb1e3fde21d08bdd23dcb5329d43ef4e455267a
김유진 <88374384+ujkkk@users.noreply.github.com>
2025-02-04 16:26:08 +0900

    [level 2] Title: 양궁대회, Time: 5.08 ms, Memory: 93.8 MB -BaekjoonHub

--- 프로그래머스/2/92342. 양궁대회/README.md
+++ 프로그래머스/2/92342. 양궁대회/README.md
@@ -1,10 +1,10 @@
 # [level 2] 양궁대회 - 92342 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/92342) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/92342#) 
 
 ### 성능 요약
 
-메모리: 80.2 MB, 시간: 3.43 ms
+메모리: 93.8 MB, 시간: 5.08 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 02월 04일 16:01:54
+2025년 02월 04일 16:26:05
 
 ### 문제 설명
 
--- 프로그래머스/2/92342. 양궁대회/양궁대회.java
+++ 프로그래머스/2/92342. 양궁대회/양궁대회.java
@@ -1,11 +1,11 @@
-// 14:38~
+// 14:38~ 16:02
 class Solution {
     static int max = 0;
     
     // max가 같을 때 
     public int[] solution(int n, int[] info) {
         int[] answer = new int[info.length];
-        dfs(0, info, 0, 0, n, new int[info.length], answer);
+        dfs(info, 0, n, new int[info.length], answer);
         
         if(max == 0){
             return new int[]{-1};
@@ -15,39 +15,25 @@ class Solution {
     }
     
     
-    void dfs(int aSum, int [] info, int depth, int sum, int curN, int [] counts, int[] answer){
-        if(depth == info.length){
-//             if(curN == 0){
-//                 for(int j=depth; j<info.length; j++){
-//                     if(info[j] == 0 && counts[j] == 0){
-//                         continue;
-//                     }
-//                     if(info[j] >= counts[j]){
-//                         aSum += (10-j);
-//                     }
-//                     else{
-//                         sum += (10-j);
-//                     }
-                    
-//                 }
-//             }
-            // System.out.println(String.format("## %d에서 끝, 차 : %d, sum : %d, aSum : %d", depth , sum - aSum, sum, aSum));
-           
-            if(aSum > sum){
+    void dfs(int [] info, int depth, int curN, int [] counts, int[] answer){
+        if(curN == 0 || depth == info.length){
+            
+            int diff = score(info, counts);
+            if(diff < 0){
                 return;
             }
             
             // 1. 가장 높은 점수로 갱신
-            if(sum - aSum > max){
+            if(diff > max){
                 
-                max = sum- aSum;
-                for(int i=0; i<counts.length; i++){
+                max = diff;
+                for(int i=0; i<info.length; i++){
                     answer[i] = counts[i];
                 }
             }
             
             // 2. 낮은 점수를 더 많이 맞힌 경우로 갱신
-            else if(max == sum - aSum ){
+            else if(max == diff){
                 for(int j = info.length -1; j>=0; j--){
                     if(answer[j] == counts[j]){
                         continue;
@@ -64,14 +50,6 @@ class Solution {
             }
             return; 
         }
-        
-         int end = sum;
-         for(int j=depth; j<info.length; j++){
-            end += (10-depth);
-        }
-        if(end < max){
-            return;
-        }
             
         // 갯수 정하기
         for(int i=info[depth]+1; i>=0; i--){
@@ -79,28 +57,27 @@ class Solution {
                 continue;
             }
             
-            if(info[depth] == 0 && i == 0){
-                sum += 0;
-            }
-            else if(i > info[depth]){    //쏜 화살이 어피치의 화살 수 보다 클 때 (승리))
-                sum += (10-depth);
-            }
-            else{
-                aSum += (10-depth);
-            }
-          
             counts[depth] = i;
-            dfs(aSum, info, depth+1, sum, curN - i, counts, answer);
+            dfs(info, depth+1, curN - i, counts, answer);
             
-            if(info[depth] == 0 && i == 0){
-                sum += 0;
-            }
-            else if(i > info[depth]){
-                sum -= (10-depth);
-            }
-            else{
-                aSum -= (10-depth);
             }
         }
+    
+    int score(int [] a1, int [] a2){
+        int apeach = 0;
+        int lion = 0;
+        
+        for(int i=0; i<a1.length; i++){
+            if(a1[i] == 0 && a2[i] == 0){
+                continue;
+            }
+            if(a1[i] >= a2[i]){
+                apeach += (10-i);
+            }else{
+                lion += (10-i);
+            }
+        }
+        
+        return lion - apeach;
     }
 }
