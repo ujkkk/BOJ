85e972d1e7fc425034d48ab78e85e115521fb790
김유진 <88374384+ujkkk@users.noreply.github.com>
2025-03-23 00:55:18 +0900

    [level 2] Title: [1차] 뉴스 클러스터링, Time: 8.08 ms, Memory: 95.7 MB -BaekjoonHub

--- 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.java
+++ 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.java
@@ -4,9 +4,8 @@ class Solution {
     public int solution(String str1, String str2) {
         HashMap<String, Integer> map = new HashMap();
         
-        int size1 = str1.length();
         int count1 = 0;
-        for(int i=0; i<size1-1; i++){
+        for(int i=0; i<str1.length()-1; i++){
             String str = str1.substring(i, i+2).toLowerCase();
             
             if(str.charAt(0) < 'a' || str.charAt(0) >'z' || str.charAt(1) < 'a' || str.charAt(1) >'z' )
@@ -15,10 +14,9 @@ class Solution {
             map.put(str, map.getOrDefault(str, 0) +1);
         }
         
-        int size2 = str2.length();
         int interCount = 0;
         int count2 = 0;
-        for(int i=0; i<size2-1; i++){
+        for(int i=0; i<str2.length()-1; i++){
             String str = str2.substring(i, i+2).toLowerCase();
             if(str.charAt(0) < 'a' || str.charAt(0) >'z' || str.charAt(1) < 'a' || str.charAt(1) >'z' )
                 continue;
@@ -41,7 +39,6 @@ class Solution {
             return 65536;
         }
         int unionCount = count1 + count2 - interCount;
-        System.out.println(String.format("%d/%d", interCount, unionCount));
         
         return interCount * 65536 / unionCount;
     }
