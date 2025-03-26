370fba1ff3a34bdc4f04d0221f8be04ef45018fe
김유진 <88374384+ujkkk@users.noreply.github.com>
2025-03-26 14:58:49 +0900

    [level 2] Title: [1차] 프렌즈4블록, Time: 24.69 ms, Memory: 86.6 MB -BaekjoonHub

--- 프로그래머스/2/17679. ［1차］ 프렌즈4블록/［1차］ 프렌즈4블록.java
+++ 프로그래머스/2/17679. ［1차］ 프렌즈4블록/［1차］ 프렌즈4블록.java
@@ -12,28 +12,15 @@ class Solution {
         }
         
         while(true){
-            checkRemoves(m, n, map);
             int removeCount = remove(m, n, map, checkRemoves(m, n, map));
-            //print(m, n, map);
             if(removeCount == 0)
                  break;
-            //System.out.println();
-            // 밑으로 내리기
             gravity(m, n, map);
-             //print(m, n, map);
             
             answer += removeCount;
         }
         
         return answer;
-    }
-    public void print(int m, int n, char [][] board){
-        for(int i=0; i<board.length; i++){
-            for(int j=0; j<board[0].length; j++){
-                System.out.print(board[i][j]);
-            }
-            System.out.println();
-        }
     }
     
     public void gravity(int m, int n, char [][] board){
