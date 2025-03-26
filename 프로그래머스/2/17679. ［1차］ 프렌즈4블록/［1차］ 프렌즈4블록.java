1ab9a429bdd2840fe3decae1b1bb473d2af83b46
김유진 <88374384+ujkkk@users.noreply.github.com>
2025-03-26 14:51:55 +0900

    [level 2] Title: [1차] 프렌즈4블록, Time: 26.90 ms, Memory: 105 MB -BaekjoonHub

---
+++ 프로그래머스/2/17679. ［1차］ 프렌즈4블록/［1차］ 프렌즈4블록.java
@@ -0,0 +1,106 @@
+import java.util.*;
+class Solution {
+    public int solution(int m, int n, String[] board) {
+        int answer = 0;
+        boolean isChange = true;
+        
+        char [][] map = new char [m][n];
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                map[i][j] = board[i].charAt(j);
+            }
+        }
+        
+        while(true){
+            checkRemoves(m, n, map);
+            int removeCount = remove(m, n, map, checkRemoves(m, n, map));
+            //print(m, n, map);
+            if(removeCount == 0)
+                 break;
+            //System.out.println();
+            // 밑으로 내리기
+            gravity(m, n, map);
+             //print(m, n, map);
+            
+            answer += removeCount;
+        }
+        
+        return answer;
+    }
+    public void print(int m, int n, char [][] board){
+        for(int i=0; i<board.length; i++){
+            for(int j=0; j<board[0].length; j++){
+                System.out.print(board[i][j]);
+            }
+            System.out.println();
+        }
+    }
+    
+    public void gravity(int m, int n, char [][] board){
+        for(int c= 0; c<n; c++){
+            char [] temp = new char[m];
+            int idx = m-1;
+            
+            Arrays.fill(temp, ' ');
+            for(int r = m-1; r>=0; r--){
+                if(board[r][c] == ' ') continue;
+                temp[idx--] = board[r][c];
+            }
+            
+            for(int i=0; i<m; i++){
+                 board[i][c] = temp[i];
+            }
+        }
+    }
+    
+    public int remove(int m, int n, char [][] board, boolean [][] isRemoves){
+        int count = 0;
+        
+         for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                if(!isRemoves[i][j])
+                    continue;
+                count++;
+                board[i][j] = ' ';
+            }
+         }
+        return count;
+    }
+    
+    public boolean [][] checkRemoves(int m, int n, char [][] board){
+        boolean [][] isRemoves = new boolean[m][n];
+        int [] dr = {0, 1, 1};
+        int [] dc = {1, 0, 1};
+        
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                if(board[i][j] == ' ') continue;
+                if(checkRemove(i, j, m, n, board)){
+                    isRemoves[i][j] = true;
+                    isRemoves[i][j+1] = true;
+                    isRemoves[i+1][j] = true;
+                    isRemoves[i+1][j+1] = true;
+                }
+            }
+        }
+        return isRemoves;
+    }
+    
+    public boolean checkRemove(int r, int c, int m, int n, char [][] board){
+        int [] dr = {0, 1, 1};
+        int [] dc = {1, 0, 1};
+        
+        for(int d=0; d<dr.length; d++){
+            int nr = r + dr[d];
+            int nc = c + dc[d];
+                    
+            if(nr < 0 || nr >= m || nc < 0 || nc >= n) 
+                return false;
+            
+            if(board[r][c] != (board[nr][nc]))
+                return false;
+                        
+        }
+        return true;
+    }
+}
