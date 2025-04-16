f1e856144bac90e18c7818a36d618464744b8389
김유진 <88374384+ujkkk@users.noreply.github.com>
2025-04-16 16:55:29 +0900

    [Silver IV] Title: 주몽, Time: 168 ms, Memory: 16436 KB -BaekjoonHub

--- 백준/Silver/1940. 주몽/주몽.java
+++ 백준/Silver/1940. 주몽/주몽.java
@@ -1,39 +1,56 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
-public class Main {
-   public static void main(String [] args) throws IOException {
-   
-	   Scanner sc = new Scanner(System.in);
-	   int N = sc.nextInt();
-	   long M = sc.nextLong();
-	   int [] num = new int[N];
-	   
-	   for(int i=0; i<N; i++) {
-		   num[i] = sc.nextInt();
-	   }
-	   
-	   //오름차순 정렬
-	   Arrays.sort(num);
-	   
-	   int start = 0;
-	   int end = N-1;
-	   int count = 0;
-	   
-	   while(start != end) {
-		   if(num[start] + num[end]> M) {
-			   end--;
-		   }else if(num[start] + num[end]< M) {
-			   start++;
-		   }else {
-			   count++;
-			   end--;
-		   }
-	   }
-	   System.out.println(count);
-   
-   
-   }
+class Main{
+    private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void main(String[] args) throws IOException {
+        int N = Integer.parseInt(br.readLine());
+        int M = Integer.parseInt(br.readLine());
+
+        int [] nums = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        for(int i=0; i<N; i++){
+            nums[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(nums);
+
+        int start = 0;
+        int end = N-1;
+        int ans = 0 ;
+
+        while(true){
+            if(start >= end) break;
+            if(start >= N || end <0) break;
+
+            int cur = nums[start] + nums[end];
+
+            if(cur > M){
+                end--;
+            }
+            else if(cur < M){
+                start++;
+            }
+            else{
+                ans++;
+                if(start+1 <N && nums[start+1] == nums[start]){
+                    start++;
+                }
+                else if(end -1 >=0 && nums[end-1] == nums[end]){
+                    end--;
+                }
+                else{
+                    start++;
+                    end--;
+                }
+            }
+        }
+
+        System.out.println(ans);
+    }
 }
