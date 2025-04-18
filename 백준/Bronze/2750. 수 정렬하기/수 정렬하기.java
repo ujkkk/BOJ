d9076995d424d03d7df9ca6f5af47083df271130
김유진 <88374384+ujkkk@users.noreply.github.com>
2025-04-18 14:50:09 +0900

    [Bronze II] Title: 수 정렬하기, Time: 108 ms, Memory: 14572 KB -BaekjoonHub

--- 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
@@ -1,35 +1,29 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
 
-public class Main {
+class Main{
+    private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-	public static void main(String[] args) throws NumberFormatException, IOException {
-		// TODO Auto-generated method stub
-		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-		int N = Integer.parseInt(bf.readLine());
-		
-		Integer [] arr = new Integer[N];
-		for(int i=0; i< N; i++) {
-			arr[i] = Integer.parseInt(bf.readLine());
-		}
-		
-		for(int i = N-1; i >=0 ; i--) {
-			int chage = 0;
-			for(int j=0; j<i; j++) {
-				if(arr[j]> arr[j+1]) {
-					chage++;
-					int tem = arr[j];
-					arr[j] = arr[j+1];
-					arr[j+1]= tem;
-				}
-			}
-			if(chage == 0) break;
-		}
-		for(int i=0;i<N;i++) {
-			System.out.println(arr[i]);
-		}
+    public static void main(String[] args) throws IOException {
 
-	}
+        int N = Integer.parseInt(br.readLine());
+        int [] nums = new int[N];
+
+        for(int i=0; i<N; i++){
+            nums[i] = Integer.parseInt(br.readLine());
+        }
+
+        Arrays.sort(nums);
+
+        StringBuilder sb = new StringBuilder();
+        for(int n : nums){
+            sb.append(n).append("\n");
+        }
+        System.out.println(sb);
+    }
 
 }
