142527f9068a8f16445b8b00735272a4764a87fb
김유진 <88374384+ujkkk@users.noreply.github.com>
2025-04-21 14:29:18 +0900

    [Gold V] Title: 신기한 소수, Time: 104 ms, Memory: 14128 KB -BaekjoonHub

--- 백준/Gold/2023. 신기한 소수/신기한 소수.java
+++ 백준/Gold/2023. 신기한 소수/신기한 소수.java
@@ -1,58 +1,52 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Scanner;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 import java.util.StringTokenizer;
 
 
-public class Main {
+class Main{
+    private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private static List<Integer> ans;
 
-    static int disit;
-    public static void main(String [] args) throws IOException{
-        Scanner sc= new Scanner(System.in);
-        disit = sc.nextInt();
-        int max = 0;
-        for(int i=1; i<disit+1; i++){
-            int disit_num = 9;
-            for(int j=1; j<i;j++)
-                disit_num*=10;
-            max += disit_num;
+
+    public static void main(String[] args) throws IOException {
+        int N = Integer.parseInt(br.readLine());
+        ans = new ArrayList<>();
+        StringBuilder sb = new StringBuilder();
+
+        for(int i=1; i<=9; i++){
+            dfs(i, 1, N);
+
         }
-
-        for(int i=2; i<=9; i++){
-            // 소수
-            if(isPrime(i)){
-                findInterstingDecimal(1, i);
-            }
+        Collections.sort(ans);
+        for(int n : ans){
+            sb.append(n).append("\n");
         }
-
-
+        System.out.println(sb);
     }
 
-    static boolean isPrime(int num){
-        for(int i =2; i<=num/2; i++){
-            if(num % i ==0)
-                return false;
-        }
-        return true;
-    }
-
-    static void findInterstingDecimal(int count,int num){
-        //count++;
-        if(count == disit){
-            if(isPrime(num)){
-                System.out.println(num);
-            }
+    public static void dfs(int n, int depth, int N){
+        if(!isDecimal(n)) return;
+        if(depth == N){
+            ans.add(n);
             return;
         }
 
         for(int i=1; i<=9; i++){
-            if(i % 2 == 0)
-                continue;
-            if(isPrime(num*10 + i)){
-                findInterstingDecimal(count+1, num*10 + i);
-            }
+            dfs(n*10 + i, depth+1, N);
         }
     }
-    
 
+
+    public static boolean isDecimal(int n){
+        if(n==1) return false;
+        for(int i=2; i*i <= n; i++){
+            if(n%i==0) return false;
+        }
+        return true;
+    }
 }
