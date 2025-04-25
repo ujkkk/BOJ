805a267f09dc28ef995c61b2f10a708e9ee64541
김유진 <88374384+ujkkk@users.noreply.github.com>
2025-04-25 16:51:13 +0900

    [Gold IV] Title: 수 묶기, Time: 104 ms, Memory: 14324 KB -BaekjoonHub

--- 백준/Gold/1744. 수 묶기/수 묶기.java
+++ 백준/Gold/1744. 수 묶기/수 묶기.java
@@ -1,81 +1,65 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
-public class Main {
 
-    static BufferedWriter bw;
-    static BufferedReader br;
-    static int zeroCount =0;
-    static int positiveCount = 0;
-    static int negativeCount=0;
+class Main{
+    private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private static int ans = 0;
+    private static int N;
+
     public static void main(String[] args) throws IOException {
 
-        br = new BufferedReader(new InputStreamReader(System.in));
-        bw = new BufferedWriter(new OutputStreamWriter(System.out));
+       // 큰거 2개를 묶는 것이 유리함 단. x*y > x+y
+        // 마이너스는 마이너스끼리, 플러스는 플러스끼리
 
         int N = Integer.parseInt(br.readLine());
-        List<Integer> nums = new ArrayList<>();
+        ArrayList<Integer> minus = new ArrayList<>();
+        ArrayList<Integer> plus = new ArrayList<>();
 
-        int ans = 0;
         for(int i=0; i<N; i++){
-            int num = Integer.parseInt(br.readLine());
-            if(num == 0){
-                zeroCount++;
-                continue;
-            } else if(num == 1){
-                // 1은 무조건 더해줌
-                ans++;
-                continue;
-            }else if(num>0){
-                positiveCount++;
-            } else{
-                negativeCount++;
+            int n = Integer.parseInt(br.readLine());
+            if(n > 0){
+                plus.add(n);
             }
-            nums.add(num);
+            else{
+                minus.add(n);
+            }
         }
-        // 오름차순 정렬
-        Collections.sort(nums);
 
-        // 음수
-        int size = nums.size();
-        int num1,num2;
-        int i;
-        
-        for(i=0; i<negativeCount; i=i+2){
-            // 음수의 개수가 홀수이면 제일 마지막 음수를 그냥 더해줌
-            if(i == negativeCount -1 && negativeCount%2 == 1){
-                // 0이 하나도 없으면 음수를 더해줌
-                if(zeroCount == 0) 
-                    ans+= nums.get(i);
+        int sum = 0;
+        // plus
+        Collections.sort(plus, Collections.reverseOrder());
+        for(int i=0; i<plus.size(); i+=2){
+            if(i == plus.size() -1){
+                sum += plus.get(i);
                 break;
             }
-            // 두 개씩 짝짓기
-            num1 = nums.get(i);
-            num2 = nums.get(i+1);
 
-            ans += num1*num2;
+            sum += Math.max(plus.get(i)*plus.get(i+1),
+                plus.get(i)+plus.get(i+1));
         }
 
-        // 양수
-        for(int j=negativeCount; j<size; j=j+2){
-            // 양수의 개수가 홀수이면 가장 앞(가장 작은 수) 수를 더하기
-            if(positiveCount%2 == 1 && j== negativeCount){
-                ans += nums.get(j);
-                j--;
-                continue;
+        // minus
+        Collections.sort(minus);
+        for(int i=0; i<minus.size(); i+=2){
+            if(i == minus.size() -1){
+                sum += minus.get(i);
+                break;
             }
-            // 두 개씩 짝짓기
-            num1 = nums.get(j);
-            num2 = nums.get(j+1);
 
-            ans += num1*num2;
+            sum += Math.max(minus.get(i)*minus.get(i+1),
+                minus.get(i)+minus.get(i+1));
         }
 
-        bw.write(ans+"\n");
-        bw.flush();
-
-        bw.close();
-        br.close();
-
+        System.out.println(sum);
     }
 }
