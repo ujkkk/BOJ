a8f7dc2244b81e8d670257b606ff4fb25085f16e
김유진 <88374384+ujkkk@users.noreply.github.com>
2025-05-01 16:45:20 +0900

    [Gold I] Title: 제곱 ㄴㄴ 수, Time: 164 ms, Memory: 15484 KB -BaekjoonHub

---
+++ 백준/Gold/1016. 제곱 ㄴㄴ 수/제곱 ㄴㄴ 수.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static void main(String[] args) throws IOException {
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        long min = Long.parseLong(st.nextToken());
+        long max = Long.parseLong(st.nextToken());
+
+        boolean A [] = new boolean[(int)(max-min+2)];
+        Arrays.fill(A, true);
+
+        // 나누어 떨어지는 수 찾기
+        int cnt = 0;
+        for(long n=2; n<=Math.sqrt(max); n++){
+            long start = (long) Math.ceil((min)*1.0/(n*n));
+
+            // j는 곱하는 값
+            for(long j=start; ; j++){
+                long cur = j*(n*n);
+                if(cur > max) break;
+                if(!A[(int)(cur-min)]) continue;
+
+                A[(int)(cur-min)] = false;
+            }
+        }
+
+        for(int i=0; i<=max-min; i++){
+            if(A[i]) cnt++;
+        }
+        System.out.println(cnt);
+    }
+}
