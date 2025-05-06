c6b9739785d2f0cf933b1e21d696fa2ad014bef4
김유진 <88374384+ujkkk@users.noreply.github.com>
2025-05-06 11:21:16 +0900

    [Gold IV] Title: 물통, Time: 124 ms, Memory: 14624 KB -BaekjoonHub

--- 백준/Gold/2251. 물통/물통.java
+++ 백준/Gold/2251. 물통/물통.java
@@ -1,118 +1,124 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.StringTokenizer;
+import java.util.Vector;
 
-import java.io.*;
-import java.util.*;
-import java.util.logging.Handler;
+class Main {
 
-public class Main {
-    static BufferedWriter bw;
-    static BufferedReader br;
-    static int sum;
-    static int [] bottle;
-    static int [] capacity;
-    static ArrayList<Integer> result;
-    static HashSet<Data> visited;
 
+    public static int N, M, K, X;
+    public static List<Integer>[] graph;
+    public static ArrayList<Integer> result = new ArrayList<>();
+    public static ArrayList<Integer> maxVisited = new ArrayList<>();
+    public static int [] max;
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     public static void main(String[] args) throws IOException {
 
-        br = new BufferedReader(new InputStreamReader(System.in));
-        bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        bottle = new int[3];
-        capacity = new int[3];
-        result = new ArrayList<>();
+       StringTokenizer st = new StringTokenizer(br.readLine());
+       max = new int[3];
+       for(int i=0; i<max.length; i++){
+           max[i] = Integer.parseInt(st.nextToken());
+       }
 
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        capacity[0] = Integer.parseInt(st.nextToken());
-        capacity[1] = Integer.parseInt(st.nextToken());
-        capacity[2] = Integer.parseInt(st.nextToken());
-
-        bottle[2] = capacity[2];
-        sum = capacity[2];
-
-        result.add(sum);
-
-        visited = new HashSet<>();
-        DFS();
-
-        Collections.sort(result);
-        for(int n : result){
-            bw.write(n+" ");
-        }
-        bw.flush();
-        br.close();
-        bw.close();
+        bfs();
 
     }
 
-    public static void DFS(){
-        if(bottle[0] == 0 && !result.contains(bottle[2])){
-            result.add(bottle[2]);
-        }
 
-        for(int i=0; i<3; i++){
-            // 옮길 물이 없으므로 패스
-            if(bottle[i]==0)
-                continue;
+    public static void bfs(){
+        HashSet<Integer> capacity = new HashSet<>();
+        HashSet<Integer> visited = new HashSet<>();
+        Queue<Integer []> que = new LinkedList<>();
+        que.add(new Integer[]{0, 0, max[2]});
 
-            for(int j= 0; j<3; j++){
-                if(i == j)
-                    continue;
 
-                int water;
-                // i병에 담겨진 물은 j병에 담을 때 초과될 때, 용량만큼만 담는다.
-                if(bottle[i]+ bottle[j] > capacity[j]){
-                    water = capacity[j]-bottle[j];
-                    bottle[i] -= water;
-                    bottle[j] += water;
+        while(!que.isEmpty()){
+            Integer [] cur = que.poll();
+            visited.add(toInt(cur));
+            if(cur[0] == 0){
+                if(capacity.contains(cur[2])) continue;
+                capacity.add(cur[2]);
+            }
 
+            // A 물통이 비어있을 때
+            if(cur[0] != max[0]){
+                // B -> A
+                if(cur[1] != 0){
+                    Integer [] l = change(1, 0, max, cur);
+                    if(!visited.contains(toInt(l)))
+                        que.add(l);
                 }
-                // i병의 물을 j병에 다 옮길 수 있을 때
-                else{
-                    water = bottle[i];
-                    bottle[j] += water;
-                    bottle[i] -= water;
+                // C -> A
+                if(cur[2] != 0){
+                    Integer [] l = change(2, 0, max, cur);
+                    if(!visited.contains(toInt(l)))
+                        que.add(l);
                 }
+            }
 
-                Data data = new Data(bottle[0], bottle[1],bottle[2] );
-                // 해보지 않는 경우에 대해서 탐색 진행
-                if(!visited.contains(data)){
-                    visited.add(data);
-                    DFS();
+            // B 물통이 비어있을 때
+            if(cur[1] != max[1]){
+                // A -> B
+                if(cur[0] != 0){
+                    Integer [] l = change(0, 1, max, cur);
+                    if(!visited.contains(toInt(l)))
+                        que.add(l);
                 }
-                // 원상복귀
-                bottle[i] += water;
-                bottle[j] -= water;
+                // C -> b
+                if(cur[2] != 0){
+                    Integer [] l = change(2, 1, max, cur);
+                    if(!visited.contains(toInt(l)))
+                        que.add(l);
+                }
+            }
+
+            // C 물통이 비어있을 때
+            if(cur[2] != max[2]){
+                // B -> C
+                if(cur[1] != 0){
+                    Integer [] l = change(1, 2, max, cur);
+                    if(!visited.contains(toInt(l)))
+                        que.add(l);
+                }
+                // A -> C
+                if(cur[0] != 0){
+                    Integer [] l = change(0, 2, max, cur);
+                    if(!visited.contains(toInt(l)))
+                        que.add(l);
+                }
             }
         }
+
+        Integer[] array =  capacity.toArray(new Integer[0]);
+        Arrays.sort(array);
+        for(int i :array){
+            System.out.print(i+" ");
+        }
+    }
+    private static int toInt(Integer [] list){
+        return list[0]*13 + list[1] *1300 + list[2]*130000;
     }
 
-}
-
-class Data{
-    int A;
-    int B;
-    int C;
-
-    public Data(int a, int b, int c){
-        this.A = a;
-        this.B = b;
-        this.C= c;
-    }
-
-    @Override
-    public boolean equals(Object data){
-        Data d = (Data)data;
-        if(d.A == A && d.B == B && d.C == C)
-            return true;
-        return false;
-    }
-
-    @Override
-    public int hashCode() {
-        int result = Integer.hashCode(A);
-        result = 31 * result + Integer.hashCode(B);
-        result = 31 * result + Integer.hashCode(C);
-        return result;
+    private static Integer [] change(int idx1, int idx2, int [] max, Integer [] cur){
+        Integer [] copy = cur.clone();
+        // idx1 -> idx2로
+        if(copy[idx2] + copy[idx1] > max[idx2]){
+            copy[idx1] = copy[idx1]- (max[idx2] - cur[idx2]);
+            copy[idx2] = max[idx2];
+        }
+        else{
+            copy[idx2] += copy[idx1];
+            copy[idx1] = 0;
+        }
+        return copy;
     }
 }
-
-
