17680dcab99dd25d4ed9bc21060060374cd41c44
김유진 <88374384+ujkkk@users.noreply.github.com>
2025-05-12 11:30:41 +0900

    [Gold III] Title: 게임 개발, Time: 216 ms, Memory: 23004 KB -BaekjoonHub

--- 백준/Gold/1516. 게임 개발/게임 개발.java
+++ 백준/Gold/1516. 게임 개발/게임 개발.java
@@ -1,81 +1,67 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
-import java.io.*;
-import java.util.*;
+class Main {
 
-/*
-    위상 정렬 이용 : 선후 관계가 있고, 사이클이 없으면서 순서를 알아야 하기 때문ㅇ
-    입력이 '[시간] [선 건물1] [선 건물2] ... [-1]' 이 주어지기에 구분 필요
-    건물은 동시에 지을 수 있기에 직전 건물까지 걸린 시간을 알고 있어야 함
-    출력 : 각 건물들의 건물들이 완성되기까지 걸린 최소 시간
- */
-public class Main {
-    static BufferedWriter bw;
-    static BufferedReader br;
-    static ArrayList<Integer> [] graph;
-    static int [] inDegree;
-    static int [] time;
-    static int N;
-
-
+    static int [] times;
+    static int [] ans;
+    static ArrayList<Integer>[] parent;
+    static int [] degree;
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     public static void main(String[] args) throws IOException {
 
-        br = new BufferedReader(new InputStreamReader(System.in));
-        bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int N = Integer.parseInt(br.readLine());
 
-        N = Integer.parseInt(br.readLine());
-        graph = new ArrayList[N+1];
-        inDegree = new int[N+1];
-        time = new int[N+1];
-
-        for(int i=0; i< graph.length; i++)
-            graph[i] = new ArrayList<Integer>();
+        parent = new ArrayList[N+1];
+        times = new int[N+1];
+        for(int i=0; i<=N; i++){
+            parent[i] = new ArrayList<>();
+        }
+        degree = new int[N+1];
+        ans = new int[N+1];
 
         for(int i=1; i<=N; i++){
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            // 건물짓는데 걸리는 시간
-            time[i] = Integer.parseInt(st.nextToken());
-            int parent;
-            while((parent = Integer.parseInt(st.nextToken())) != -1){
-                graph[parent].add(i);
-                inDegree[i]++;
+            String [] st =br.readLine().split(" ");
+            for(int j=0; j<st.length-1; j++){
+                if(j == 0){
+                    times[i] = Integer.parseInt(st[j]);
+                    continue;
+                }
+                int c = Integer.parseInt(st[j]);
+                parent[c].add(i);
+                degree[i]++;
             }
         }
-        int [] result =  topologicalSorting();
 
+        Queue<Integer> que = new LinkedList<>();
         for(int i=1; i<=N; i++){
-            bw.write(result[i] + time[i] + "\n");
-        }
-
-        bw.flush();
-        br.close();
-        bw.close();
-
-    }
-
-    public static int [] topologicalSorting(){
-        int [] result = new int[N+1];
-        Queue<Integer> que = new LinkedList();
-        for(int i=1; i< inDegree.length; i++){
-            if(inDegree[i] == 0){
+            if(degree[i] == 0){
                 que.add(i);
             }
         }
 
-        while(!que.isEmpty()) {
+        StringBuilder sb = new StringBuilder();
+        while(!que.isEmpty()){
             int cur = que.poll();
-            for (int child : graph[cur]) {
-                inDegree[child]--;
-                // 업데이트
-                result[child] = Math.max(result[cur] + time[cur], result[child]);
-                // 만족시켜 할 선후관계가 없기에 삽입
-                if (inDegree[child] == 0) {
-                    // 더 짧은 시간이면 갱신
-                    que.add(child);
+            ans[cur] += times[cur];
+
+            for(int next : parent[cur]){
+                ans[next] = Math.max(ans[cur], ans[next]);
+
+                if(--degree[next] == 0){
+                    que.add(next);
                 }
             }
         }
-        return result;
+        for(int i=1; i<=N; i++){
+            sb.append(ans[i]).append("\n");
+        }
+        System.out.println(sb);
     }
-
 
 }
