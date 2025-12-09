72b6ed0c75cd11dcb4a648fd952632e8f55a188f
김유진 <88374384+ujkkk@users.noreply.github.com>
2025-12-09 21:09:56 +0900

    [level 1] Title: 실패율, Time: 5.80 ms, Memory: 85.4 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/실패율.java
+++ 프로그래머스/1/42889. 실패율/실패율.java
@@ -1,52 +1,6 @@
-import java.util.*;
-
 class Solution {
     public int[] solution(int N, int[] stages) {
-        
-        int [] failCounts = new int[N+1];
-        int allCleCount = 0;
-        
-        // 스테이지마다 실패 수 구하기
-        for(int i=0; i<stages.length; i++){
-            if(stages[i] == N+1){
-                continue;
-            }
-            failCounts[stages[i]]++;
-        }
-        
-        // 실패율 구하기
-        ArrayList<Stage> list = new ArrayList();
-        int failCount = 0;
-        for(int i=1; i<=N; i++){
-            double loss = failCounts[i] == 0? 0.0 : (double)failCounts[i] / ((stages.length) - failCount);
-            failCount += failCounts[i];
-            //System.out.println(i+" 스테이지 실패율 : " + loss);
-            list.add(new Stage(i, loss));
-        }
-        
-        Collections.sort(list);
-        int[] answer = new int[N];
-        for(int i=0; i<N; i++){
-            answer[i] = list.get(i).n;
-        }
+        int[] answer = {};
         return answer;
-    }
-}
-
-class Stage implements Comparable<Stage>{
-    int n;
-    double loss;
-    
-    public Stage(int n, double loss){
-        this.n = n;
-        this.loss = loss;
-    }
-    
-    @Override
-    public int compareTo(Stage o){
-        if(this.loss == o.loss){
-            return this.n - o.n;
-        }
-        return Double.compare(o.loss, this.loss);
     }
 }
