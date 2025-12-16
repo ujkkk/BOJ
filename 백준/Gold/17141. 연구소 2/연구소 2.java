6758c05f28a341bb972a387a3f532b84e49dd2cb
김유진 <88374384+ujkkk@users.noreply.github.com>
2025-12-16 20:30:54 +0900

    [Gold IV] Title: 연구소 2, Time: 228 ms, Memory: 41704 KB -BaekjoonHub

--- 백준/Gold/17141. 연구소 2/연구소 2.java
+++ 백준/Gold/17141. 연구소 2/연구소 2.java
@@ -1,140 +1,127 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
-public class Main{
-    //시계 방향
-    public static int [] dr = {-1, 0, 1, 0};
-    public static int [] dc = {0, 1, 0, -1};
-    public static boolean [][] isVisited;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
-    public static BufferedReader br;
-    public static BufferedWriter bw;
-    public static int N;
-    public static int M;
-    public static int exceptionCount= 0;
+class Main {
 
-    public static List<Point> virusPosList;
-    public static boolean map[][];
-    public static int result = Integer.MAX_VALUE-1;
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static int N, M;
+    static int [][] map;
+    static  ArrayList<Point> viruses;
+    static  int wallCount = 0;
 
-    public static void main(String [] args) throws Exception {
-        br = new BufferedReader(new InputStreamReader(System.in));
-        bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static int MAX_VALUE = 100000;
+    static int min = MAX_VALUE;
+
+    public static void main(String[] args) throws IOException {
 
         StringTokenizer st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
 
-        map = new boolean[N][N];
-        virusPosList = new ArrayList<>();
-        initMap();
+        map = new int[N][N];
+        viruses = new ArrayList<>();
 
-        //int virusSize = Math.min(M, virusPosList.size());
-        boolean [] isSelected = new boolean [virusPosList.size()];
-        Point[] selectedVirus  = new Point[M];
-        selectVirusPosition(0, isSelected,selectedVirus, 0);
-
-        if(result == Integer.MAX_VALUE -1)
-            bw.write("-1\n");
-        else
-            bw.write(result-1 +"\n");
-        bw.flush();
-
-    }
-
-    public static void selectVirusPosition(int depth, boolean [] isSelected, Point[] selectedVirus, int start){
-        if(depth == M){
-            // 퍼지는 최소 일 구하기
-            boolean [][] isVisited = new boolean[N][N];
-            getMinDay(selectedVirus, isVisited);
-            return;
-        }
-
-        for(int i=start; i< isSelected.length; i++){
-            if(!isSelected[i]){
-                selectedVirus[depth] = virusPosList.get(i);
-                isSelected[i] = true;
-
-                selectVirusPosition(depth +1, isSelected,selectedVirus, i+1);
-
-                isSelected[i] = false;
-                selectedVirus[depth] = null;
+        for(int i=0; i<N; i++){
+            st = new StringTokenizer(br.readLine());
+            for(int j=0;j<N; j++){
+                map[i][j] = Integer.parseInt(st.nextToken());
+                if(map[i][j] == 2){
+                    viruses.add(new Point(i,j));
+                }
+                else if(map[i][j] == 1){
+                    wallCount++;
+                }
             }
         }
+        // 입력 끝
+
+        // 1. 바이러스 M개 선택
+        selectVirus(0, 0, new boolean[viruses.size()], new Point[M]);
+        System.out.println(min == MAX_VALUE? -1 : min -1);
     }
 
-    public static void getMinDay(Point[] selectedVirus,  boolean [][] isVisited){
+    public static int simulation(Point [] select){
         Queue<Point> que = new LinkedList<>();
-        int day = 0;
-        int count = 0;
+        boolean [][] isVisited = new boolean[N][N];
+        int time = 0;
+        int pollutionCount = 0;
+
         int [] dr = {1, 0, -1, 0};
         int [] dc = {0, 1, 0, -1};
-        for(Point virus : selectedVirus){
-            que.add(virus);
-            isVisited[virus.row][virus.col] = true;
+
+        // 바이러스 위치 삽입
+        for(Point virus : select){
+            que.add(new Point(virus.r, virus.c));
+            isVisited[virus.r][virus.c] = true;
+            
+            pollutionCount++;
         }
 
         while(!que.isEmpty()){
-            day++;
+            if(time > N*N){
+                break;
+            }
+
             int size = que.size();
             for(int i=0; i<size; i++){
                 Point cur = que.poll();
-                count++;
 
-                for(int j=0; j<dr.length ;j++){
-                    int nextRow = cur.row + dr[j];
-                    int nextCol = cur.col + dc[j];
+                for(int j=0; j<4; j++){
+                    int nr = cur.r + dr[j];
+                    int nc = cur.c + dc[j];
 
-                    // && !isVisited[nextRow][nextCol]
-                    if(isRange(nextRow, nextCol) && map[nextRow][nextCol]&& !isVisited[nextRow][nextCol]){
-                        que.add(new Point(nextRow, nextCol));
-                        isVisited[nextRow][nextCol] = true;
-
+                    if(nr < 0|| nr >= N || nc <0 || nc >= N){
+                        continue;
                     }
+                    if(isVisited[nr][nc] || map[nr][nc] == 1){
+                        continue;
+                    }
+                    que.add(new Point(nr, nc));
+                    pollutionCount++;
+                    isVisited[nr][nc] = true;
                 }
             }
+            time++;
         }
 
-        if(count < N*N-exceptionCount)
+        return (pollutionCount == (N*N-wallCount))? time : MAX_VALUE;
+    }
+
+    public static void selectVirus(int depth, int next, boolean [] isSelected, Point [] select){
+        if(depth == M){
+            min = Math.min(min, simulation(select));
             return;
-        result = Math.min(result, day);
-    }
-
-    public static void initMap() throws IOException {
-        for(int i=0; i<N; i++){
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            for(int j=0; j<N; j++){
-                int value = Integer.parseInt(st.nextToken());
-                switch (value){
-                    case 0: // 빈 칸
-                        map[i][j] = true;
-                        break;
-                    case 1: // 벽
-                        map[i][j] = false;
-                        exceptionCount++;
-                        break;
-                    case 2: // 바이러스가 놓아질 수 있는 위치
-                        map[i][j] = true;
-                        virusPosList.add(new Point(i,j));
-                }
-            }
         }
-    }
 
-    public static boolean isRange(int r, int c){
-        if(r >= 0 && r < N  && c >=0 && c< N)
-            return true;
-        return false;
+        for(int i=next; i<viruses.size(); i++){
+            if(isSelected[i]){
+                continue;
+            }
+
+            select[depth] = viruses.get(i);
+            isSelected[i] = true;
+
+            selectVirus(depth+1, i+1, isSelected, select);
+
+            isSelected[i] = false;
+        }
     }
 
 }
 
-class Point{
-    int row;
-    int col;
 
-    public Point(int row, int col){
-        this.row = row;
-        this.col =col;
+class Point {
+    int r;
+    int c;
+
+    public Point(int r, int c) {
+        this.r = r;
+        this.c = c;
     }
 }
