ea258fdbfdcdb0beb0d1a86297a6f3ba2948c0fc
김유진 <88374384+ujkkk@users.noreply.github.com>
2026-03-04 17:11:35 +0900

    [Silver I] Title: 단축키 지정, Time: 100 ms, Memory: 14176 KB -BaekjoonHub

--- 백준/Silver/1283. 단축키 지정/단축키 지정.java
+++ 백준/Silver/1283. 단축키 지정/단축키 지정.java
@@ -1,114 +1,93 @@
+import javax.print.attribute.standard.PresentationDirection;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-class Solution {
+// 1. 단축키 중복을 관리하는 저장소
+class ShortcutRegistry {
+    private final boolean[] used = new boolean[26];
 
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-    int N;
-    String [] strs;
-    boolean [] isUsed = new boolean[26];
-
-    void run() throws IOException{
-        input();
-        solution();
+    public boolean isAvailable(char c){
+        int idx = Character.toLowerCase(c) - 'a';
+        return !used[idx];
     }
 
-    private void input() throws IOException {
-        N = Integer.parseInt(br.readLine());
-        strs = new String[N];
-        for(int i=0; i<N; i++){
-            strs[i] = br.readLine();
-        }
+    public void register(char c){
+        int index = Character.toLowerCase(c) - 'a';
+        used[index] = true;
+    }
+}
+
+// 2. 하나의 메뉴 옵션
+class MenuOption {
+    private final String rawText;
+    private int shrtcutIndex = -1;
+
+    public MenuOption(String rawText){
+        this.rawText = rawText;
     }
 
-    private void solution(){
-        StringBuilder sb = new StringBuilder();
-        for(String str : strs){
-
-            // 1. 단어 단위로
-            String result = selectKeyByWord(str);
-            if(result.equals("")){
-                // 2. 알파멧 단위로
-                result = selectKeyByAlphabet(str);
-            }
-
-            sb.append(result.equals("")? str : result).append("\n");
-        }
-        System.out.println(sb);
+    public void assignShortcut(ShortcutRegistry registry){
+        if (tryAssignFirstLetters(registry)) return;
+        tryAssignAnyLetters(registry);
     }
 
-    /*
-        단어 단위로 넘기면서 단축키 찾음
-     */
-    String selectKeyByWord(String str){
-        StringBuilder sb = new StringBuilder();
-
-        String [] words = str.split(" ");
-        boolean isFind = false;
+    private boolean tryAssignFirstLetters(ShortcutRegistry registry){
+        String[] words = rawText.split(" ");
+        int curIdx = 0;
 
         for(String word : words){
-            char firstAlpha = word.charAt(0);
-            int key = Character.toLowerCase( firstAlpha) - 'a';
+            char firstChar = word.charAt(0);
+            if(registry.isAvailable(firstChar)){
+                registry.register(firstChar);
+                this.shrtcutIndex = curIdx;
+                return true;
+            }
+            curIdx += word.length() + 1;
+        }
+        return false;
+    }
 
-            if(isFind || isUsed[key]){
-                sb.append(word).append(" ");
+    private void tryAssignAnyLetters(ShortcutRegistry registry){
+        for(int i=0; i<rawText.length(); i++){
+            char c = rawText.charAt(i);
+            if(c == ' '){
                 continue;
             }
-
-            // 단축키 찾음
-            isUsed[key] = true;
-            isFind = true;
-            sb.append("[").append(firstAlpha).append("]");
-            for(int i=1; i<word.toCharArray().length; i++){
-                sb.append(word.charAt(i));
+            if(registry.isAvailable(c)){
+                registry.register(c);
+                this.shrtcutIndex = i;
+                return;
             }
-            sb.append(" ");
         }
-        return isFind? sb.toString() : "";
     }
 
+    @Override
+    public String toString(){
+        if(shrtcutIndex == -1) return rawText;
 
-    String selectKeyByAlphabet(String str){
-        StringBuilder sb = new StringBuilder();
-
-        String [] words = str.split(" ");
-        boolean isFind = false;
-
-        for(String word : words){
-            for(char alpha : word.toCharArray()){
-                int key = Character.toLowerCase(alpha) - 'a';
-
-                if(isUsed[key] || isFind){
-                    sb.append(alpha);
-                    continue;
-                }
-
-                // 단축키 찾음
-                isUsed[key] = true;
-                isFind = true;
-                sb.append("[").append(alpha).append("]");
-            }
-            sb.append(" ");
-        }
-        return isFind? sb.toString() : "";
+        StringBuilder sb = new StringBuilder(rawText);
+        sb.insert(shrtcutIndex+1,"]");
+        sb.insert(shrtcutIndex, "[");
+        return sb.toString();
     }
-
 }
 
 public class Main {
 
-    /*
-        단축키는 알파벳 하나.
-        1. 단어 단위로 끊음. 모든 구별은 소문자로
-        2. 알파벳 단위로 끊음.
-        출력 : 단축키로 지정된 알파멧 위치에 '[]' 표시 해야 함
-     */
-    public static void main(String[] args) throws IOException {
+    public static void main(String [] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
 
-        new Solution().run();
+        ShortcutRegistry registry = new ShortcutRegistry();
+        StringBuilder result = new StringBuilder();
 
+        for(int i=0; i<n; i++){
+            MenuOption option = new MenuOption(br.readLine());
+            option.assignShortcut(registry);
+            result.append(option).append("\n");
+        }
+
+        System.out.println(result);
     }
-
 }
