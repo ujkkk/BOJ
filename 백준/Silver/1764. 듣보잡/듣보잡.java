12c2d39863fcb9de33a8267f3583546be43e63fc
김유진 <88374384+ujkkk@users.noreply.github.com>
2026-03-10 16:39:53 +0900

    [Silver IV] Title: 듣보잡, Time: 280 ms, Memory: 28960 KB -BaekjoonHub

--- 백준/Silver/1764. 듣보잡/듣보잡.java
+++ 백준/Silver/1764. 듣보잡/듣보잡.java
@@ -1,49 +1,47 @@
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
 
 
-class Main
-{
-    public static HashSet<Character> set;
-    public static void main(String args[]) throws Exception {
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    public static void main(String[] args) throws IOException {
 
-        String [] data = br.readLine().split(" ");
-        int N = Integer.parseInt(data[0]);
-        int M = Integer.parseInt(data[1]);
 
-        HashSet<String> names = new HashSet<>();
+        HashSet<String> set1 = new HashSet<>();
+        HashSet<String> set2 = new HashSet<>();
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
         for(int i=0; i<N; i++){
-            names.add(br.readLine());
+            set1.add(br.readLine());
         }
-
-        List<String> nameList = new ArrayList<>();
         for(int i=0; i<M; i++){
-            String name = br.readLine();
-            if(names.contains(name)){
-                nameList.add(name);
+            set2.add(br.readLine());
+        }
+
+        ArrayList<String> list = new ArrayList<>();
+        for(String name1 : set1){
+            if(set2.contains(name1)){
+                list.add(name1);
             }
         }
-        nameList.sort(Comparator.naturalOrder());
 
-        bw.write(nameList.size() + "\n");
-        nameList.forEach(name -> {
-            try {
-                bw.write(name+"\n");
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-        });
+        Collections.sort(list);
+        StringBuilder result = new StringBuilder();
 
-        bw.flush();
-
-        bw.close();
-        br.close();
-
+        result.append(list.size()).append("\n");
+        for(String name : list){
+            result.append(name).append("\n");
+        }
+        System.out.println(result);
     }
 
-}
 
+
+}
