e95e0d7087f186c4595f2e6606e81f8abac0c33b
김유진 <88374384+ujkkk@users.noreply.github.com>
2026-03-10 16:46:12 +0900

    [Silver IV] Title: ATM, Time: 112 ms, Memory: 14412 KB -BaekjoonHub

--- 백준/Silver/11399. ATM/ATM.java
+++ 백준/Silver/11399. ATM/ATM.java
@@ -1,32 +1,33 @@
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.util.*;
 
-class Main{
-    private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
     public static void main(String[] args) throws IOException {
 
+        // 최소 - 짧은 것부터 앞에 세우기
         int N = Integer.parseInt(br.readLine());
-        int [] nums = new int[N];
-
         StringTokenizer st = new StringTokenizer(br.readLine());
+        int nums [] = new int[N];
         for(int i=0; i<N; i++){
             nums[i] = Integer.parseInt(st.nextToken());
         }
-
         Arrays.sort(nums);
 
-        int sum = 0;
-        int result = 0;
+        int prefixSum = 0;
+        int curSum = 0;
         for(int i=0; i<N; i++){
-            sum += nums[i];
-            result += sum;
+            curSum += nums[i];
+            prefixSum += curSum;
         }
-
-        System.out.println(result);
+        System.out.println(prefixSum);
     }
+
+
 
 }
