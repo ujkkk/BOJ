866eae2c664bd6652b605903da91959545e7b985
김유진 <88374384+ujkkk@users.noreply.github.com>
2026-03-10 17:10:19 +0900

    [Silver III] Title: 1로 만들기, Time: 132 ms, Memory: 18176 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -1,40 +1,30 @@
-import java.io.*;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
 
+
 public class Main {
-	
-	public static BufferedWriter bw;
-	public static BufferedReader br;
-	
-	static int [] dp;
-	static int N;
-	
-	public static void main(String[] args) throws Exception {
-		
-		BufferedReader br  = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-	
-		N = Integer.parseInt(br.readLine());	
-		dp = new int[N*3+1];
-		Arrays.fill(dp, Integer.MAX_VALUE);
-		dp[1] = 0;
-		
-		for(int i=2; i<=N; i++) {
-			dp[i] = Math.min(dp[i], dp[i-1]+1);
-			if(i % 2 == 0) {
-				dp[i] = Math.min(dp[i], dp[i/2]+1);
-			}
-			if(i % 3 == 0) {
-				dp[i] = Math.min(dp[i], dp[i/3]+1);
-			}
-		}
-		System.out.print(dp[N]);
-	
-	
-	}
-		
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+
+    public static void main(String[] args) throws IOException {
+
+        int N = Integer.parseInt(br.readLine());
+        int [] dp = new int[N+1]; //  1에서 n을 만들기까지 최소 연산 수
+        Arrays.fill(dp, 1_000_010);
+        dp[N] = 0;
+
+        // 1_000_000 (백만)
+        for(int i=N; i>=1; i--) {
+            dp[i-1] = Math.min(dp[i-1], dp[i] + 1);
+            if(i%2 == 0)
+                dp[i/2] = Math.min(dp[i/2], dp[i] + 1);
+            if(i%3 == 0)
+                dp[i/3] = Math.min(dp[i/3], dp[i] + 1);
+        }
+        System.out.println(dp[1]);
+    }
+
 }
-
-
-
