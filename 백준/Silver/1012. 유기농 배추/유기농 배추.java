d750f111cbe3f7c1cac805204ca86e2f5c36c0c7
김유진 <88374384+ujkkk@users.noreply.github.com>
2026-03-11 13:43:49 +0900

    [Silver II] Title: 유기농 배추, Time: 148 ms, Memory: 16012 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.java
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.java
@@ -1,102 +1,77 @@
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.Scanner;
-import java.util.StringTokenizer;
+
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.FileInputStream;
+import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import java.util.*;
 
-class Main
-{
-	static boolean isBaecu [][];
-	public static void main(String args[]) throws Exception
-	{
-		
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		
-		int T = Integer.parseInt(br.readLine());
-		
-		
-		for(int t = 0; t<T; t++) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			// 가로 길이
-			int M = Integer.parseInt(st.nextToken());
-			// 세로 길이
-			int N = Integer.parseInt(st.nextToken());
-			// 배추 위치 개
-			int K = Integer.parseInt(st.nextToken());
-			
-			// 배추밭 정보 생성
-			isBaecu = new boolean[N+2][M+2];
-			
-			for(int k=0; k<K; k++) {
-				// 배추 위치 정보
-				st = new StringTokenizer(br.readLine());
-				int x = Integer.parseInt(st.nextToken());
-				int y = Integer.parseInt(st.nextToken());
-				isBaecu[y+1][x+1] = true;
-			}
-			
-			bw.write(solution() + "\n");
-		
-			
-		}
-		bw.flush();
-	}
-	
-	public static int solution(){
-		int row = isBaecu.length;
-		int col = isBaecu[0].length;
-		int count = 0;
-		
-		boolean [][] isVisited = new boolean[row][col];
-		
-		for(int y=1; y<row; y++) {
-			for(int x = 1; x<col; x++) {
-				// 새로운 그룹 발견
-				if(!isVisited[y][x] && isBaecu[y][x] == true) {
-					searchGroup(isVisited, x, y);
-					count++;
-				}
-			}
-		}
-		return count;
-	}
-	
-	// 한 그룹 탐색
-	public static void searchGroup(boolean [][] isVisited, int x, int y) {
-		Queue<Point> que = new LinkedList<>();
-		int [] dx = {0,1,0,-1};
-		int [] dy = {1,0, -1,0};
-		
-		que.add(new Point(x,y));
-		isVisited[y][x] = true;
-		
-		while(!que.isEmpty()) {
-			Point cur = que.poll();
-			
-			for(int i=0; i<4; i++) {
-				int nextX = cur.x+dx[i];
-				int nextY = cur.y+dy[i];
-		
-				if(!isVisited[nextY][nextX]&& isBaecu[nextY][nextX] == true) {
-					que.add(new Point(nextX, nextY));
-					isVisited[nextY][nextX] = true;
-				}
-			}
-		}
-	}
 
-}
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-class Point{
-	int x;
-	int y;
-	public Point(int x, int y) {
-		this.x = x;
-		this.y = y;
-	}
+    static int N;
+    static ArrayList<Integer>[] graph;
+    public static void main(String[] args) throws IOException {
+
+        int T = Integer.parseInt(br.readLine());
+        StringBuilder result = new StringBuilder();
+        while(T-- > 0){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int C = Integer.parseInt(st.nextToken());
+            int R = Integer.parseInt(st.nextToken());
+            int K = Integer.parseInt(st.nextToken());
+
+            boolean [][] map = new boolean[R][C];
+
+            // 맵 구성
+            for(int k=0; k<K; k++){
+                st = new StringTokenizer(br.readLine());
+                int c = Integer.parseInt(st.nextToken());
+                int r = Integer.parseInt(st.nextToken());
+                map[r][c] = true;
+            }
+
+            boolean [][] isVisited = new boolean[R][C];
+            int ans = 0;
+
+            for(int r=0; r<R; r++){
+                for(int c=0; c<C; c++){
+                    if(isVisited[r][c] || !map[r][c]) continue;
+                    isVisited[r][c]= true;
+                    bfs(map, new int[]{r,c}, isVisited);
+                    ans++;
+                }
+            }
+            result.append(ans).append("\n");
+
+        }
+        System.out.println(result);
+    }
+
+    public static void bfs(boolean [][] map, int [] start, boolean [][] isVisited){
+        Queue<int[]> que = new ArrayDeque();
+        que.add(start);
+
+        int [] dr = {-1, 0 ,1 ,0};
+        int [] dc = {0, 1, 0, -1};
+        int R = map.length;
+        int C = map[0].length;
+
+        while(!que.isEmpty()){
+            int [] cur = que.poll();
+
+            for(int d=0; d<4; d++){
+                int nr = cur[0] + dr[d];
+                int nc = cur[1] + dc[d];
+
+                if(nr < 0 || nr >=R || nc < 0 || nc >=C)
+                    continue;
+                if(isVisited[nr][nc] || !map[nr][nc])
+                    continue;
+
+                isVisited[nr][nc] = true;
+                que.add(new int[]{nr, nc});
+            }
+        }
+    }
+
 }
